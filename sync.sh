c4bba08934cddc75217c3429bb947e4d30f029db
Sunho Kim <sunho@Sunhos-MacBook-Pro.local>
2023-07-10 14:07:12 -0700

    Initial

---
+++ sync.sh
@@ -0,0 +1,7 @@
+cp ~/.zshrc .
+cp ~/.config/nvim/coc-settings.json .
+cp ~/.config/nvim/init.vim .
+cp ~/.config/karabiner/karabiner.json .
+cp ~/.config/kitty/kitty.conf .
+cp ~/.config/vifm/vifmrc  .
+
